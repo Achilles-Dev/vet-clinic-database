19de466e9256dc7e7d6938d681ab2ba81cbc902b
Solomon Achilles <solohag92@gmail.com>
2022-05-30 17:30:34 +0000

    Update schema | data | queries files

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL, 
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL
+    weight_kg DECIMAL,
+    PRIMARY KEY (id)
 );
