4461d4cb62025d93f54baf3b95ae39ce9ca52869
Solomon Achilles <solohag92@gmail.com>
2022-06-01 10:14:56 +0000

    Add more data to animals table

--- data.sql
+++ data.sql
@@ -11,3 +11,14 @@ VALUES ('Pikachu', '2021-01-07', 1, False, 15.04);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', '2017-05-12', 5, True, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', '2020-02-08', 0, False, -11),
+        ('Plantmon', '2021-11-15', 2, True, -5.7),
+        ('Squirtle', '1993-04-02', 3, False, -12.13),
+        ('Angemon', '2005-06-12', 1, True, -45),
+        ('Boarmon', '2005-06-07', 7, True, 20.4),
+        ('Blossom', '1998-10-13', 3, True, 17),
+        ('Ditto', '2022-05-14', 4, True, 22);
+
+
--- schema.sql
+++ schema.sql
@@ -7,5 +7,6 @@ CREATE TABLE animals (
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
+    species VARCHAR(100),
     PRIMARY KEY (id)
 );
