eefd54c36b1943aad605492da522d8ff8209ebab
Solomon Achilles <solohag92@gmail.com>
2022-06-01 19:59:25 +0000

    Add new tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,24 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(255);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD CONSTRAINT animal_species FOREIGN KEY (species_id)
+    REFERENCES species (id);
+
+ALTER TABLE animals ADD CONSTRAINT animal_owner FOREIGN KEY (owner_id)
+    REFERENCES owners (id);
