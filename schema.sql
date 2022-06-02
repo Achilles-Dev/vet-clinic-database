7d59b1dbbfbb09050ec3e9075417c298c2c230b1
Solomon Achilles <solohag92@gmail.com>
2022-06-02 08:28:03 +0000

    Update schema file

--- schema.sql
+++ schema.sql
@@ -46,15 +46,20 @@ CREATE TABLE vets (
 );
 
 CREATE TABLE specializations (
+    id INT GENERATED ALWAYS AS IDENTITY,
     species_id INT,
     vet_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (species_id) REFERENCES species (id),
     FOREIGN KEY (vet_id) REFERENCES vets (id)
 );
 
 CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    date_of_visit DATE,
     animal_id INT,
     vet_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (animal_id) REFERENCES animals (id),
     FOREIGN KEY (vet_id) REFERENCES vets (id)
 );
